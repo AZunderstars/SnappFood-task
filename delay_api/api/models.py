cdd89434a663a56e52220128933fe12da4ad1b2e
AZunderstars <fidchetspinner@gmail.com>
2024-06-13 00:05:44 +0330

    add Order model

--- delay_api/api/models.py
+++ delay_api/api/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
 class Vendor(models.Model):
     id = models.AutoField(primary_key=True)
 
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.PROTECT, related_name="orders", related_query_name="order")
+    register_time = models.DateTimeField(auto_now_add=True)
+    delivery_time = models.PositiveIntegerField()
