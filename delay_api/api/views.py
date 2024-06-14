e0d048970b02fb88eef64f2c758b01071de59d76
AZunderstars <fidchetspinner@gmail.com>
2024-06-14 12:45:40 +0330

    refactor views and add error checks

--- delay_api/api/views.py
+++ delay_api/api/views.py
@@ -1,3 +1,4 @@
+from django.core import serializers
 from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
@@ -7,8 +8,12 @@ from django.utils import timezone
 from .models import *
 import json
 import requests
+import queue
+
 
 url = "https://run.mocky.io/v3/122c2796-5df4-461c-ab75-87c1192b17f7"
+
+default_minute_latency_to_add = 15
 
 
 def get_new_arrive_time():
@@ -17,7 +22,7 @@ def get_new_arrive_time():
         return response.json().get("new_arrive_time")
 
     except (requests.exceptions.RequestException, requests.exceptions.JSONDecodeError):
-        return timezone.localtime(timezone.now() + timezone.timedelta(minutes=15))
+        return timezone.localtime(timezone.now() + timezone.timedelta(minutes=default_minute_latency_to_add))
 
 
 def push_to_delay_queue(order):
@@ -29,6 +34,24 @@ def has_trip_on_the_way(order):
     return hasattr(order, "trip") and order.trip.status != "DELIVERED"
 
 
+def pop_from_delay_queue():
+    con = get_redis_connection("default")
+    return con.rpop('delay_queue')
+
+
+def is_delay_queue_empty():
+    con = get_redis_connection("default")
+    return con.llen('delay_queue') == 0
+
+
+def get_order_from_delay_queue():
+    if is_delay_queue_empty():
+        raise queue.Empty
+    order_id = pop_from_delay_queue()
+    order = Order.objects.get(id=order_id)
+    return order
+
+
 @csrf_exempt
 @require_POST
 def delay_report_announce(request):
@@ -38,26 +61,34 @@ def delay_report_announce(request):
         if order.is_now_late_for_delivery():
             return HttpResponse('not late', status=400)
         if has_trip_on_the_way(order):
-            new_arrive_time = get_new_arrive_time()
+            if hasattr(order, "employee"):
+                return HttpResponse('order is being examined by an employee', status=400)
             DelayReport.objects.create(order=order, action="RESCHEDULED")
-            return HttpResponse({"new_arrive_time": new_arrive_time})
+            new_arrive_time = get_new_arrive_time()
+            return JsonResponse({"new_arrive_time": new_arrive_time})
         else:
             DelayReport.objects.create(order=order, action="DELAY_QUEUED")
             push_to_delay_queue(order)
+            return HttpResponse("queued to examine by an employee")
 
-    except (json.decoder.JSONDecodeError, Order.DoesNotExist, KeyError):
-        pass
+    except (json.decoder.JSONDecodeError, KeyError, Order.DoesNotExist):
+        return HttpResponse('bad request', status=400)
 
 
 @csrf_exempt
 @require_GET
 def get_delay_report_from_queue(request):
-    body = json.loads(request.body)
-    if "id" not in body:
-        return HttpResponse('bad parameters')
-    con = get_redis_connection("default")
-    id = con.rpop('delay_queue')
-    return HttpResponse(id)
+    try:
+        body = json.loads(request.body)
+        employee = Employee.objects.get(id=body["id"])
+        if employee.working_on is not None:
+            return HttpResponse('another order already assigned', status=400)
+        order = get_order_from_delay_queue()
+        employee.working_on = order
+        employee.save()
+        return HttpResponse(serializers.serialize('json', [order]), content_type='application/json')
+    except (json.decoder.JSONDecodeError, KeyError, Employee.DoesNotExist, queue.Empty, Order.DoesNotExist):
+        return HttpResponse('bad request', status=400)
 
 
 @csrf_exempt
@@ -66,4 +97,6 @@ def get_vendors_delay_report(request):
     vendors = Vendor.objects \
         .annotate(num_delays=Count("order__delay_report"))\
         .order_by("-num_delays")
-    return JsonResponse(list(vendors.values()), safe=False)
+    data = serializers.serialize('json', vendors)
+    return HttpResponse(data, content_type='application/json')
+    # return JsonResponse(list(vendors.values()), safe=False)
